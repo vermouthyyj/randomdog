68ce450137cb07bbdb2e19a84aac215d0d4003bd
Yujun Yan <yujunyan@Yujuns-MacBook-Pro.local>
2020-10-31 02:10:47 +1100

    fix deploy bugs

--- my-app/src/HomePage/HomePage.js
+++ my-app/src/HomePage/HomePage.js
@@ -95,7 +95,8 @@ class HomePage extends Component {
     render() {
         var { isLoaded, dogPictures } = this.state;
         if (!isLoaded) {
-            return <CircleToBlockLoading></CircleToBlockLoading>;
+            // return <CircleToBlockLoading></CircleToBlockLoading>;
+            return <h1>Loading...</h1>
         }
         else {
             return (
